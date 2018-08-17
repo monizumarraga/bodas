f9565d76bf987dc1f5344ce080f0630484dcc97d
EC2 Default User <ec2-user@ip-172-31-26-83.eu-west-1.compute.internal>
2018-08-17 07:17:02 +0000

    info navigation

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,6 +1,5 @@
 class Information < ApplicationRecord
     belongs_to :user
-    validates :info, presence: true, length: {minimum: 3, maximum: 1000}
     validates :name, presence: true, length: {minimum: 3, maximum: 1000}
     validates :city, presence: true, length: {minimum: 3, maximum: 100}
     
